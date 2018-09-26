e8497950bd54e1a3bb33551977b654c406dbc679
Paul Fell <swellpf@gmail.com>
2018-09-26 18:44:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(number)
-  if num <= 1
+  if number <= 1
     false
-  elsif num = 2
-    true
-  elsif
-    (2..num-1).to_a.all? do |possible_factor|
+  else
+    (2..number-1).to_a.all? do |possible_factor|
       num % possible_factor != 0
       end
   end
